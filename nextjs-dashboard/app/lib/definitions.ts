40ce9b43f684e79cd319122615c0a05b18b4a9b1
JaredMorillo <jared.morillo@hotmail.com>
2026-01-29 13:30:54 -0500

    Nextjs Tutorial

--- nextjs-dashboard/app/lib/definitions.ts
+++ nextjs-dashboard/app/lib/definitions.ts
@@ -17,9 +17,9 @@ export type Customer = {
 };
 
 export type Invoice = {
-  id: string; // Will be created on the database
+  id: string;
   customer_id: string;
-  amount: number; // Stored in cents
+  amount: number;
   date: string;
   // In TypeScript, this is called a string union type.
   // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
